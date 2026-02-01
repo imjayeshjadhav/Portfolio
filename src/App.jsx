e33207e55b56171dbf887065e4c116088deed311
imjayeshjadhav <14jayeshjadhav@gmail.com>
2026-02-02 01:50:24 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from '#components/Navbar'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl text-white font-bold underline'>Welcome to my portfolio</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
